ac992dda1fd7341bfe6a90bd3c9bf7d529ff21f8
JaiDhiman <jaidhiman2000@gmail.com>
2025-04-04 04:10:13 -0700

    removed tests for now

--- apps/mobile/babel.config.js
+++ apps/mobile/babel.config.js
@@ -4,7 +4,7 @@ module.exports = function (api) {
     presets: [["babel-preset-expo", { jsxImportSource: "nativewind" }], "nativewind/babel"],
     plugins: [
       "react-native-reanimated/plugin",
-      '@babel/plugin-transform-runtime',
+      "@babel/plugin-transform-runtime",
       [
         "module:react-native-dotenv",
         {
--- apps/mobile/src/test/__mocks__/envMock.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  API_URL: "https://test-api.example.com",
-};
